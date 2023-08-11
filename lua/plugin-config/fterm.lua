8ae52525a18cdfa02b44ad9b58e4f1ae95489e8b
NOK <potatochick2020@gmail.com>
2023-08-11 17:46:08 +0800

    Change key of float term , add default gitui_theme.ron

--- lua/plugin-config/fterm.lua
+++ lua/plugin-config/fterm.lua
@@ -30,5 +30,5 @@ vim.keymap.set('n', '<A-g>', function()
     gitui:toggle()
 end)
 
-vim.keymap.set('n', '<A-i>', '<CMD>lua require("FTerm").toggle()<CR>')
-vim.keymap.set('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set('n', '<A-t>', '<CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set('t', '<A-t>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
