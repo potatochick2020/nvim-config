e87c2ed73c3eb28934484f9964e73f541661fbb6
NOK <potatochick2020@gmail.com>
2024-08-20 16:57:33 +0800

    change lvim config

--- unix/lvim/config.lua
+++ unix/lvim/config.lua
@@ -36,6 +36,8 @@
 --command shortcut
 vim.cmd [[command! Qa :qa]]
 vim.cmd [[command! Q :q]]
+vim.cmd [[command! Wq :wq]]
+vim.cmd [[command! WQ :wq]]
 vim.cmd [[command! ScriptRemoveAllEmptyLines :g/^$/d]]
 vim.cmd [[command! ScriptRemoveMultipleEmptyLines :g/^\_$\n\_^$/d]]
 -- NvimTree toggle to <F5>
@@ -101,6 +103,9 @@ lvim.keys.insert_mode["<C-j>"] = "<C-o>j"
 
 -- auto close lvim
 lvim.plugins = {
+    -- Golang
+    "olexsmir/gopher.nvim",
+    "leoluz/nvim-dap-go",
     {
         "phaazon/hop.nvim",
         event = "BufRead",
@@ -251,3 +256,99 @@ vim.wo.wrap = true
 vim.o.whichwrap = 'b,s,<,>,[,],h,l'
 -- search during typing
 vim.o.incsearch = true
+-- #Go lang
+------------------------
+-- Treesitter
+------------------------
+lvim.builtin.treesitter.ensure_installed = {
+    "go",
+    "gomod",
+}
+
+------------------------
+-- Formatting
+------------------------
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+    { command = "goimports", filetypes = { "go" } },
+    { command = "gofumpt",   filetypes = { "go" } },
+}
+
+lvim.format_on_save = {
+    pattern = { "*.go" },
+}
+
+------------------------
+-- Dap
+------------------------
+local dap_ok, dapgo = pcall(require, "dap-go")
+if not dap_ok then
+    return
+end
+
+dapgo.setup()
+
+------------------------
+-- LSP
+------------------------
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "gopls" })
+
+local lsp_manager = require "lvim.lsp.manager"
+lsp_manager.setup("golangci_lint_ls", {
+    on_init = require("lvim.lsp").common_on_init,
+    capabilities = require("lvim.lsp").common_capabilities(),
+})
+
+lsp_manager.setup("gopls", {
+    on_attach = function(client, bufnr)
+        require("lvim.lsp").common_on_attach(client, bufnr)
+        local _, _ = pcall(vim.lsp.codelens.refresh)
+        local map = function(mode, lhs, rhs, desc)
+            if desc then
+                desc = desc
+            end
+
+            vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc, buffer = bufnr, noremap = true })
+        end
+        map("n", "<leader>Ci", "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies")
+        map("n", "<leader>Ct", "<cmd>GoMod tidy<cr>", "Tidy")
+        map("n", "<leader>Ca", "<cmd>GoTestAdd<Cr>", "Add Test")
+        map("n", "<leader>CA", "<cmd>GoTestsAll<Cr>", "Add All Tests")
+        map("n", "<leader>Ce", "<cmd>GoTestsExp<Cr>", "Add Exported Tests")
+        map("n", "<leader>Cg", "<cmd>GoGenerate<Cr>", "Go Generate")
+        map("n", "<leader>Cf", "<cmd>GoGenerate %<Cr>", "Go Generate File")
+        map("n", "<leader>Cc", "<cmd>GoCmt<Cr>", "Generate Comment")
+        map("n", "<leader>DT", "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test")
+    end,
+    on_init = require("lvim.lsp").common_on_init,
+    capabilities = require("lvim.lsp").common_capabilities(),
+    settings = {
+        gopls = {
+            usePlaceholders = true,
+            gofumpt = true,
+            codelenses = {
+                generate = false,
+                gc_details = true,
+                test = true,
+                tidy = true,
+            },
+        },
+    },
+})
+
+local status_ok, gopher = pcall(require, "gopher")
+if not status_ok then
+    return
+end
+
+gopher.setup {
+    commands = {
+        go = "go",
+        gomodifytags = "gomodifytags",
+        gotests = "gotests",
+        impl = "impl",
+        iferr = "iferr",
+    },
+}
+-- # Install in first boot
+--:MasonInstall gopls golangci-lint-langserver delve goimports gofumpt gomodifytags gotests impl
